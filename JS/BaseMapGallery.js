dbaf421b0641443bfbefc9d304b56c457211e846
justingreco <justin.greco@raleighnc.gov>
2015-01-30 12:32:36 -0500

    initial commit

---
+++ JS/BaseMapGallery.js
@@ -0,0 +1,205 @@
+﻿//function for adding the basemap layers
+function CreateBaseMapComponent() {
+    if (baseMapLayerCollection.length != 0) {
+		
+        for (var i = 0; i < baseMapLayerCollection.length; i++) {
+            map.addLayer(CreateBaseMapLayer(baseMapLayerCollection[i].MapURL, baseMapLayerCollection[i].Key, (i == 0) ? true : false));
+        }
+		//var labels = new esri.layers.ArcGISDynamicMapServiceLayer('http://maps.raleighnc.gov:4443/ArcGIS/rest/services/Labels/MapServer');
+		//map.addLayer(labels, map.layerIds.length - 1);		
+        if (baseMapLayerCollection.length == 1) {
+            dojo.byId('divBaseMapTitleContainer').style.display = 'none';
+            HideLoadingMessage();
+            return;
+        }
+
+        var layerList = dojo.byId('layerList');
+
+        for (var i = 0; i < Math.ceil(baseMapLayerCollection.length / 2); i++) {
+            var previewDataRow = document.createElement("tr");
+
+            if (baseMapLayerCollection[(i * 2) + 0]) {
+                var layerInfo = baseMapLayerCollection[(i * 2) + 0];
+                layerList.appendChild(CreateBaseMapElement(layerInfo));
+            }
+
+            if (baseMapLayerCollection[(i * 2) + 1]) {
+                var layerInfo = baseMapLayerCollection[(i * 2) + 1];
+                layerList.appendChild(CreateBaseMapElement(layerInfo));
+            }
+        }
+        if (dojo.isIE != 7) {
+            dojo.addClass(dojo.byId("imgThumbNail" + baseMapLayerCollection[0].Key), "selectedBaseMap");
+        }
+        if (dojo.isIE == 8) {
+            dojo.byId("imgThumbNail" + baseMapLayerCollection[0].Key).style.marginTop = "-5px";
+            dojo.byId("imgThumbNail" + baseMapLayerCollection[0].Key).style.marginLeft = "-5px";
+            dojo.byId("spanBaseMapText" + baseMapLayerCollection[0].Key).style.marginTop = "8px";
+        }
+
+		
+    }
+    else {
+        ShowDialog('Error', 'No basemap layer found. Atleast one basemap layer is required. Please contact your administrator.');
+        HideLoadingMessage();
+    }
+}
+
+//function for changing the map onclick
+function CreateBaseMapElement(baseMapLayerInfo) {
+    var divContainer = document.createElement("div");
+    divContainer.className = "baseMapContainerNode";
+    var imgThumbnail = document.createElement("img");
+    imgThumbnail.src = baseMapLayerInfo.ThumbnailSource;
+    imgThumbnail.className = "basemapThumbnail";
+    imgThumbnail.id = "imgThumbNail" + baseMapLayerInfo.Key;
+    imgThumbnail.setAttribute("layerId", baseMapLayerInfo.Key);
+    imgThumbnail.onclick = function () {
+        ChangeBaseMap(this);
+    };
+    var spanBaseMapText = document.createElement("span");
+    var spanBreak = document.createElement("br");
+    spanBaseMapText.id = "spanBaseMapText" + baseMapLayerInfo.Key;
+    spanBaseMapText.className = "basemapLabel";
+    spanBaseMapText.innerHTML = baseMapLayerInfo.Name;
+    divContainer.appendChild(imgThumbnail);
+    divContainer.appendChild(spanBreak);
+    divContainer.appendChild(spanBaseMapText);
+    return divContainer;
+}
+
+//function for showing the present map and hiding previous map on window
+function ChangeBaseMap(spanControl) {
+    dijit.byId('imgBaseMap').attr("checked", false);
+    HideMapLayers();
+    var key = spanControl.getAttribute('layerId');
+    for (var i = 0; i < baseMapLayerCollection.length; i++) {
+        dojo.removeClass(dojo.byId("imgThumbNail" + baseMapLayerCollection[i].Key), "selectedBaseMap");
+        dojo.byId("imgThumbNail" + baseMapLayerCollection[i].Key).style.marginTop = "0px";
+        dojo.byId("imgThumbNail" + baseMapLayerCollection[i].Key).style.marginLeft = "0px";
+        dojo.byId("spanBaseMapText" + baseMapLayerCollection[i].Key).style.marginTop = "1px";
+        if (baseMapLayerCollection[i].Key == key) {
+            if (dojo.isIE != 7) {
+                dojo.addClass(dojo.byId("imgThumbNail" + baseMapLayerCollection[i].Key), "selectedBaseMap");
+            }
+            if (dojo.isIE == 8) {
+                dojo.byId("imgThumbNail" + baseMapLayerCollection[i].Key).style.marginTop = "-5px";
+                dojo.byId("imgThumbNail" + baseMapLayerCollection[i].Key).style.marginLeft = "-5px";
+                dojo.byId("spanBaseMapText" + baseMapLayerCollection[i].Key).style.marginTop = "8px";
+            }
+            var layer = map.getLayer(baseMapLayerCollection[i].Key);
+            ShowHideBaseMapComponent();
+            layer.show();
+        }
+    }
+}
+
+//function for displaying a map on window
+function CreateBaseMapLayer(layerURL, layerId, isVisible) {
+	if (layerURL.indexOf('MapServer') > -1)
+	{
+		var layer = new esri.layers.ArcGISTiledMapServiceLayer(layerURL, { id: layerId, visible: isVisible });	
+	}
+	else if (layerURL.indexOf('ImageServer') > -1)
+	{
+		var layer = new esri.layers.ArcGISTiledMapServiceLayer(layerURL, { id: layerId, visible: isVisible });	
+	}
+	
+
+    return layer;
+}
+
+//function for hiding a map on window
+function HideMapLayers() {
+    for (var i = 0; i < baseMapLayerCollection.length; i++) {
+        var layer = map.getLayer(baseMapLayerCollection[i].Key);
+        if (layer) {
+            layer.hide();
+        }
+    }
+}
+
+//function for showing and hiding of basemap container
+function ShowHideBaseMapComponent() {
+    if (dojo.byId('divAddressContainer').children.length != 0) {
+        WipeOutControl(dojo.byId('divAddressContainer'), 500);
+        setTimeout(function () { RemoveChildren(dojo.byId('divAddressContainer')); }, 500);
+    }
+    else {
+        dojo.byId('divAddressContainer').style.display = 'none';
+    }
+    var node = dojo.byId('divBaseMapTitleContainer');
+	HideLayersWidget();
+    var appNode = dojo.byId('divAppContainer');
+    if (dojo.coords(appNode).h > 0) {
+        dijit.byId('imgapplink').attr("checked", false);
+        WipeOutControl(appNode, 500);
+    }
+
+    if (dojo.coords(node).h > 0) {
+        WipeOutControl(node, 500);
+    }
+    else {
+        WipeInControl(node, 500);
+    }
+}
+
+
+//function for adding the basemap layers
+function CreateLayersComponent() {
+    if (opLayerCollection.length != 0) {
+        //var layerList = dojo.byId('layersList');
+		var layerList = new dojox.layout.TableContainer({
+								cols:1
+							},dojo.byId('layersList'));
+		
+        for (var i = 0; i < opLayerCollection.length; i++) {
+			var isVisible = false;
+			if (opLayerCollection[i].Visible == "true"){
+				isVisible = true;
+			}
+			var cb = new dijit.form.CheckBox({
+				name:"layerCb"+i, 
+				id:"layerCb"+i, 
+				value:opLayerCollection[i].Key, 
+				title:opLayerCollection[i].Name, 
+				checked:isVisible,
+				onChange: function(b){
+					map.getLayer(this.value).setVisibility(this.checked);
+				}});
+			//var label = dojo.create('label', {"for":"layerCb"+i, "innerHTML":opLayerCollection[i].Name});
+			//cb.placeAt("layersList","first");
+			layerList.addChild(cb);
+        }
+		layerList.startup();
+    }
+    else {
+        ShowDialog('Error', 'No basemap layer found. Atleast one layer is required. Please contact your administrator.');
+        HideLoadingMessage();
+    }
+}
+
+function ShowHideLayersComponent(){
+    if (dojo.byId('divAddressContainer').children.length != 0) {
+        WipeOutControl(dojo.byId('divAddressContainer'), 500);
+        setTimeout(function () { RemoveChildren(dojo.byId('divAddressContainer')); }, 500);
+    }
+    else {
+        dojo.byId('divAddressContainer').style.display = 'none';
+    }
+	var node = dojo.byId('divLayersTitleContainer');
+	HideBaseMapWidget();
+	var appNode = dojo.byId('divAppContainer');
+	    if (dojo.coords(appNode).h > 0) {
+        dijit.byId('imgapplink').attr("checked", false);
+        WipeOutControl(appNode, 500);
+    }
+
+    if (dojo.coords(node).h > 0) {
+        WipeOutControl(node, 500);
+    }
+    else {
+        WipeInControl(node, 500);
+    }
+}
+
---
+++ JS/Route.js
@@ -0,0 +1,202 @@
+﻿//function for configuring the route betwwen two points
+
+function ConfigureRoute(mapPoint) {
+    ShowLoadingMessage("Loading...");
+    ShowDojoLoading(dojo.byId('scrollbar_container2'));
+    routeParams.stops.features = [];
+    routeParams.stops.features[0] = new esri.Graphic(mapPoint, null);
+	
+	if (pollName != ''){
+		var query = new esri.tasks.Query();
+		var queryTask = new esri.tasks.QueryTask('http://maps.raleighnc.gov/ArcGIS/rest/services/Parks/ParkLocator/MapServer/1');	
+		query.where = "NAME = '"+pollName+"'";
+		query.returnGeometry = true;
+		queryTask.execute(query, function(result){
+			if (result.features.length == 0){
+			    routeParams.stops.features[1] = new esri.Graphic(pollPoint, null);
+				if (routeParams.stops.features.length === 2) {
+					routeTask.solve(routeParams);
+				}
+			}
+			else if (result.features.length > 1){
+			    //routeParams.stops.features[1] = new esri.Graphic(result.features[0].geometry, null);
+				//if (routeParams.stops.features.length === 2) {
+				//	routeTask.solve(routeParams);
+				//}
+				FindClosestFacility(mapPoint, result.features);
+			}
+			else if (result.features.length == 1)
+			{
+				routeParams.stops.features[1] = new esri.Graphic(result.features[0].geometry, null);
+				if (routeParams.stops.features.length === 2) {
+					routeTask.solve(routeParams);
+				}			
+			}
+			//If both the "to" and the "from" addresses are set, solve the route
+
+		});
+	}
+	else{
+		routeParams.stops.features[1] = new esri.Graphic(pollPoint, null);
+		   //If both the "to" and the "from" addresses are set, solve the route
+		if (routeParams.stops.features.length === 2) {
+			routeTask.solve(routeParams);
+		}
+	}
+
+	
+	
+
+}
+
+function FindClosestFacility(from, toGraphics){
+	params = new esri.tasks.ClosestFacilityParameters();
+	params.defaultCutoff = 50.0;
+	params.returnIncidents = false;
+	params.returnRoutes = true;
+	params.returnDirections = false;
+	params.defaultTargetFacilityCount = 1;
+	params.directionsTimeAttribute = "";
+	var features = [];
+	features.push(new esri.Graphic(from));
+	var incidents = new esri.tasks.FeatureSet();
+	incidents.features = features;
+	params.incidents = incidents;
+	features = [];
+	for (var i=0;i<toGraphics.length;i++){
+		features.push(new esri.Graphic(toGraphics[i].geometry,null));
+	}
+	var facilities = new esri.tasks.FeatureSet();
+	facilities.features = features;
+	params.facilities = facilities;
+	var closestFacilityTask = new esri.tasks.ClosestFacilityTask("http://maps.raleighnc.gov/ArcGIS/rest/services/Networks/WakeNetwork/NAServer/Closest%20Facility");
+	closestFacilityTask.solve(params, function(solveResult){
+			    routeParams.stops.features[1] = new esri.Graphic(toGraphics[solveResult.routes[0].attributes['FacilityID']-1].geometry,null);
+				if (routeParams.stops.features.length === 2) {
+					routeTask.solve(routeParams);
+				}
+		}, function(fault){
+			var tst = "";
+	});
+}
+
+var pollRoute;
+var routeText = " ";
+//function for displaying the route between two points
+function ShowRoute(solveResult) {
+	
+    map.graphics.clear(routeSymbol);
+    var directions = solveResult.routeResults[0].directions;
+	pollRoute = directions.mergedGeometry;
+    //Add route to the map
+    map.graphics.add(new esri.Graphic(directions.mergedGeometry, routeSymbol));
+    //if (!layer) {
+        map.setExtent(directions.mergedGeometry.getExtent().expand(3));
+    //}
+    //Display the total time and distance of the route
+    dojo.byId("tdParkDirections").innerHTML = "Total distance: " + FormatDistance(directions.totalLength, "mile(s)") + "<br />Total time: " + FormatTime(directions.totalTime);
+	routeText = dojo.byId("tdParkDirections").innerHTML.replace('<br>',';');
+    if (!layer) {
+        dojo.byId('directionContainer').style.display = 'block';
+    }
+    var tableDir;
+    var tBodyDir;
+    if (!dojo.byId('tblDir')) {
+        tableDir = document.createElement('table');
+        tBodyDir = document.createElement('tbody');
+        tableDir.id = 'tblDir';
+        tableDir.style.width = "95%";
+        tBodyDir.id = 'tBodyDir';
+        tableDir.cellSpacing = 0;
+        tableDir.cellPadding = 0;
+        tableDir.appendChild(tBodyDir);
+    }
+    else {
+        tableDir = dojo.byId('tblDir');
+        tBodyDir = dojo.byId('tBodyDir');
+    }
+
+    dojo.forEach(solveResult.routeResults[0].directions.features, function (feature, i) {
+        var miles = FormatDistance(feature.attributes.length, "miles");
+        var trDir = document.createElement('tr');
+        trDir.style.verticalAlign = 'top';
+        tBodyDir.appendChild(trDir);
+        var tdDirNum = document.createElement('td');
+        tdDirNum.innerHTML = (i + 1) + ". &nbsp";
+        trDir.appendChild(tdDirNum);
+        var tdDirVal = document.createElement('td');
+        tdDirVal.style.paddingBottom = '5px';
+        if (i == 0) {
+            tdDirVal.innerHTML = feature.attributes.text.replace('Location 1', map.getLayer(tempGraphicsLayerId).graphics[0].attributes.Address);
+        }
+        else if (i == (solveResult.routeResults[0].directions.features.length - 1)) {
+            tdDirVal.innerHTML = feature.attributes.text.replace('Location 2', dojo.byId('leftInfoWindowHeader').innerHTML);                    
+        }
+        else {
+            if (miles) {
+                tdDirVal.innerHTML = feature.attributes.text + " (" + FormatDistance(feature.attributes.length, "miles") + ")";
+            }
+            else {
+                tdDirVal.innerHTML = feature.attributes.text;
+            }
+        }
+		routeText+= ';<BOL>'+(i+1)+".</BOL> "+tdDirVal.innerHTML;
+        trDir.appendChild(tdDirVal);
+    });
+    dojo.byId("direction").appendChild(tableDir);
+    CreateScrollbar(dojo.byId('directionContainer'), dojo.byId('direction'));
+	if(ismobile){
+		dojo.style('direction','overflow','auto');}
+    HideDojoLoading();
+    HideLoadingMessage();
+}
+
+//Display any errors that were caught when attempting to solve the rotue
+function ErrorHandler(err) {
+    dojo.byId('divLoadMessage').innerHTML = err.message + "\n" + err.details.join("\n");
+    dijit.byId('dialogLoadMessage').titleNode.innerHTML = 'Error';
+    dijit.byId('dialogLoadMessage').show();
+    HideDojoLoading();
+    HideLoadingMessage();
+    if (!layer) {
+        dojo.byId('tdParkDirections').innerHTML = '';
+        dojo.byId('directionContainer').style.display = 'none';
+        map.graphics.clear();
+        map.getLayer(tempGraphicsLayerId).clear();
+        mapPoint = '';
+    }
+    if (dijit.byId('btnCurrentLocation')) {
+        if (dijit.byId('btnCurrentLocation').checked) {
+            dijit.byId('btnCurrentLocation').setAttribute('checked', false);
+            dojo.byId('txtDAddress').disabled = false;
+            dojo.byId('txtDAddress').title = 'Enter an address to locate';
+            dojo.byId('directionSearch').style.cursor = 'pointer';
+            dojo.byId('directionSearch').title = "Search";
+        }
+    }
+}
+
+//Format the time as hours and minutes
+function FormatTime(time) {
+    var hr = Math.floor(time / 60), //Important to use math.floor with hours
+            min = Math.round(time % 60);
+
+    if (hr < 1 && min < 1) {
+        return "";
+    }
+    else if (hr < 1) {
+        return min + " minute(s)";
+    }
+
+    return hr + " hour(s) " + min + " minute(s)";
+}
+
+//Round the distance to the nearest hundredth of a unit
+function FormatDistance(dist, units) {
+    var d = Math.round(dist * 100) / 100;
+    if (d === 0) {
+        return "";
+    }
+
+    return d + " " + units;
+}
